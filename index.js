3029ee844c16c19df25da9375969fef7d8a28e6b
Anna Wilson <ajoyna01@gmail.com>
2021-11-11 11:41:34 -0600

    incomplete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+let reqBody = {
+  name: "Sam",
+  email: "sam@sam.com"
+};
+ function submitData(reqBody) { 
+   
+  fetch("http://localhost:3000/users",{
+   method: "POST",
+   headers: {
+     "Content-Type": "application/json",
+     "Accept": "application/json"
+   },
+   body: JSON.stringify(submitData)
+  })
+ .then(resp => resp.json())
+  .then(submitData =>
+   console.log(submitData))
+  .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+  }
